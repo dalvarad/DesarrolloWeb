0ace4cb4a37afc03ca33753f7bca6515021bfd35
Fernando <fpmartin@alumnos.ubiobio.cl>
2017-07-05 18:15:55 -0400

    reparado nav para movil, agregado js-3.2.1, eliminado css, SOLO Bootstratp

--- resources/views/template/partials/nav.blade.php
+++ resources/views/template/partials/nav.blade.php
@@ -1,28 +1,26 @@
-<nav class="navbar navbar-default navbar-static-top">     
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toogle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">     
-                                                                                                                       
-                <span class="sr-only"> Este boton despliega la barra de navegacion</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Menu</a>
-        </div>
-        
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-center">
-                <li><a href="{{url('/')}}"><span class="icon-home3"></span> Inicio</a></li>
-                <li><a href="{{url('quienessomos')}}"><span class="icon-earth"></span> ¿Quiénes somos?</a></li>                
-                <li><a href="{{url('hotel')}}"><span class="glyphicon glyphicon-header"></span> Hotel</a></li>
-                <li><a href="{{url('habitaciones')}}"><span class="icon-briefcase"></span> Habitaciones</a></li>
-                
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="icon-rocket"></span> Log In</a></li>
-                <li><a href="{{url('contacto')}}"><span class="icon-mail3"></span> Contactos</a></li>
-            </ul>
-        </div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
     </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="{{url('/')}}"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
+        <li><a href="{{url('quienessomos')}}"><span class="glyphicon glyphicon-sunglasses"></span> ¿Quiénes somos?</a></li>                
+        <li><a href="{{url('hotel')}}"><span class="glyphicon glyphicon-header"></span> Hotel</a></li>
+        <li><a href="{{url('habitaciones')}}"><span class="glyphicon glyphicon-bed"></span> Habitaciones</a></li> 
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-off"></span> Log In</a></li>
+        <li><a href="{{url('contacto')}}"><span class="glyphicon glyphicon-envelope"></span> Contactenos</a></li>
+      </ul>
+    </div>
+  </div>
 </nav>
