c90760771a7f0a7d6abe8733aeafa277a07fecaa
Fernando <fpmartin@alumnos.ubiobio.cl>
2017-07-06 19:33:19 -0400

    implementado faker para crear usuarios

--- database/seeds/UsuariosSeeder.php
+++ database/seeds/UsuariosSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class UsuariosSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class UsuariosSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\Usuario::class, 10)->create();
     }
 }
