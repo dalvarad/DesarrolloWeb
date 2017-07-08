174179ccd8666bd0c2111140aa20777a8e47bfa1
Fernando <fpmartin@alumnos.ubiobio.cl>
2017-07-08 03:38:04 -0400

    validacion extra en el controller cliente-usuario, faker Clientes

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -18,23 +18,35 @@ use \Freshwork\ChileanBundle\Rut;
 $factory->define(App\Usuario::class, function (Faker\Generator $faker) {
  
     return [
-        'nombre_usuario' => $faker->name,
-        'rut_usuario' => Rut::set(rand(1000000, 25000000))->fix()->format(Rut::FORMAT_WITH_DASH),
-        'usuario' => $faker->userName,
-        'pass' => bcrypt(str_random(10)),
-        'tipo' => 'recepcionista'
+        'nombre_usuario'    => $faker->name,
+        'rut_usuario'       => Rut::set(rand(1000000, 25000000))->fix()->format(Rut::FORMAT_WITH_DASH),
+        'usuario'           => $faker->userName,
+        'pass'              => bcrypt(str_random(10)),
+        'tipo'              => 'recepcionista'
     ];
 
 });
 
 
-
 $factory->define(App\Habitacion::class, function (Faker\Generator $faker) {
  
     return [
-        'valor' => $faker->numberBetween($min = 5000, $max = 250000),
-        'estado' => $faker->randomElement($array = array ('ocupada','desocupada')),
-        'tipo_de_habitacion' =>$faker->randomElement($array = array ('single','matrimonial','double')) 
+        'valor'                 => $faker->numberBetween($min = 5000, $max = 250000),
+        'estado'                => $faker->randomElement($array = array ('ocupada','desocupada')),
+        'tipo_de_habitacion'    => $faker->randomElement($array = array ('single','matrimonial','double')) 
+    ];
+
+});
+
+$factory->define(App\Cliente::class, function (Faker\Generator $faker) {
+ 
+    return [
+        'nombre_cliente'    => $faker->name,
+        'rut_cliente'       => Rut::set(rand(1000000, 25000000))->fix()->format(Rut::FORMAT_WITH_DASH),
+        'direccion'         => $faker->streetAddress,
+        'telefono'          => $faker->numberBetween($min = 50000000, $max = 99999999),
+        'usuario'           => $faker->userName,
+        'pass'              => bcrypt(str_random(10)),
     ];
 
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsuariosSeeder::class);
     	$this->call(HabitacionesSeeder::class);
+        $this->call(ClientesSeeder::class);
     }
 }
